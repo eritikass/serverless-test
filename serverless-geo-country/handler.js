beba32573669d1f69234995a86fafcc8b18ece57
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2019-01-28 10:29:44 +0200

    use arrow function

--- serverless-geo-country/handler.js
+++ serverless-geo-country/handler.js
@@ -1,12 +1,10 @@
 'use strict';
 
-module.exports.country = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      ok: true,
-      viewer_country_code: event.headers['CloudFront-Viewer-Country'] || '',
-      viewer_ip: event.requestContext && event.requestContext.identity && event.requestContext.identity['sourceIp'] || '',
-    }),
-  };
-};
+module.exports.country = async event => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    ok: true,
+    viewer_country_code: event.headers['CloudFront-Viewer-Country'] || '',
+    viewer_ip: event.requestContext && event.requestContext.identity && event.requestContext.identity['sourceIp'] || '',
+  }),
+});
